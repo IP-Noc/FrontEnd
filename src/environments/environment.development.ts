4f51a36a08fef3717bd9d43cfe69aa4f9bf2a381
yassineMessaoud9 <98484999+yassineMessaoud9@users.noreply.github.com>
2024-03-22 02:17:53 +0100

    fix noc

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
   BASE_URL: 'https://ipnoc2.onrender.com',
-  //BASE_URL:'http://localhost:3000'
+ // BASE_URL:'http://localhost:3000'
 
 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
 BASE_URL: 'https://ipnoc2.onrender.com',
- //BASE_URL:'http://localhost:3000'
+// BASE_URL:'http://localhost:3000'
 
 };
